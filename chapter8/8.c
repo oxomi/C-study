0e338318143d52efaead8339835150a78fd358d2
oxomi <osm0071@naver.com>
2023-08-10 14:02:46 +0900

    .

---
+++ chapter8/8.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//	int ary[5];
+//
+//	ary[0] = 10;
+//	ary[1] = 20;
+//	ary[2] = ary[0] + ary[1];
+//	scanf("%d", &ary[3]);
+//
+//	printf("%d\n", ary[2]);
+//	printf("%d\n", ary[3]);
+//	printf("%d\n", ary[4]);
+//
+//	return 0;
+//
+//}
+
+
+
+//int main()
+//{
+//	int score[5];
+//	int i;
+//	int total = 0;
+//	double avg;
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		scanf("%d", &score[i]);
+//	}
+//
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		total += score[i];
+//	}
+//
+//	avg = total / 5.0;
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%5d", score[i]);
+//	}
+//	printf("\n");
+//
+//	printf("ЦђБе: %.1lf", avg);
+//
+//	return 0;
+//}
+
+
+ 
