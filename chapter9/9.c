dd0c937e3efd45b3f11e2bb171a51c25990f0351
oxomi <osm0071@naver.com>
2023-08-16 17:07:20 +0900

    수정

--- chapter9/9.c
+++ chapter9/9.c
@@ -70,8 +70,87 @@
 //}
 
 
+//void swap(int* pa, int* pb);
+//
+//int main()
+//{
+//	int a = 10, b = 20;
+//
+//	swap(&a, &b); //pa변수에는 주솟값을 넣어야 하므로 인수에 &a를 넣어야 하는 것. 그래야 swap함수에서 간접 참조 연산을 수행할 수 있게 됨.
+//	printf("a:%d, b:%d", a, b);
+//
+//	return 0;
+//}
+//
+//
+//void swap(int* pa, int* pb)
+//{
+//	int temp;
+//
+//	temp = *pa;
+//	*pa = *pb;
+//	*pb = temp;
+//
+//}
 
 
 
+//Q. 포인터 없이 두 변수의 값을 바꾸는 함수는 불가능한가?
+
+//A. 불가능 case 1) swap의 인수가 없을때
+//void swap(void);
+//
+//int main()
+//{
+//	int a = 10, b = 20;
+//
+//	swap();
+//	printf("a:%d, b:%d\n", a, b);
+//
+//	return 0;
+//}
+//
+//
+//void swap(void)
+//{
+//	int temp;
+//
+//	temp = a;
+//	a = b;
+//	b = temp;
+//}
+
+//에러발생. 
+//이유 : 함수 안에 선언된 함수명은 사용 번위가 함수 내부로 제한되므로 main함수에 있는 변수 a, b는 다른 함수인 swap내에서 사용 불가.
 
 
+//A. 불가능 case 2) swap의 인수가 있을때
+void swap(int x, int y);
+
+int main()
+{
+	int a = 10, b = 20;
+
+	swap(a, b);
+	printf("a:%d, b:%d", a, b);
+
+	return 0;
+}
+
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+//a, b값이 바뀌지 않음.
+//이유: swap 함수를 호출 할 때, main함수의 a, b값이 복사되어 swap함수의 x,y에 저장되므로 결국 swap함수 안에서는 a, b의 복사본을 바꾸는게 되기 때문에
+//정작 main함수의 a, b값은 바뀌지 않게 된다. swap함수의 매개변수 이름을 a, b로 바꾸얻 마찬가지다.
+
+
+//A. 불가능 case 3) case 2에서 swap의 바꾼 값을 main함수로 반환하도록 수정했을때
+//애초에 불가능함.
+//이유: 함수는 오직 하나의 값만을 반환할 수 있기 떄분에 두개의 변화값을 반환할 수가 없다.
